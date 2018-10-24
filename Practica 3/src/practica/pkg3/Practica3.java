d9bbd79960078c0bafc53a2acf528147b1a0a5c6
Albert <Albertpm95@gmail.com>
2018-10-24 13:49:11 +0200

    Añadido Leer de archivo y guardar en archivo

--- Practica 3/src/practica/pkg3/Practica3.java
+++ Practica 3/src/practica/pkg3/Practica3.java
@@ -1,11 +1,38 @@
 package practica.pkg3;
 
-public class Practica3 {
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Practica3 implements Serializable{
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        ByteArrayInputStream bs;
+        ObjectInputStream is;
+        FileInputStream entrada;
+        Entorno entorno;
         
-        Entorno entorno = new Entorno();
-        entorno.CrearEntorno();
+        try
+        {
+            entrada = new FileInputStream("Entorno.dat");
+            is = new ObjectInputStream(entrada);
+            entorno = (Entorno)is.readObject();
+            is.close();
+        }catch (FileNotFoundException e){entorno = new Entorno(); entorno.CrearEntorno();}
+    }
     
+    void GuardarEstado(Entorno entorno) throws IOException
+    {
+        FileOutputStream salida = new FileOutputStream("Entorno.dat");
+        try (ObjectOutputStream os = new ObjectOutputStream (salida)) 
+        {
+            os.writeObject(entorno);  
+        }
     }
 }
