c8ecc446dbeeb6055b1b3371ebf661455b0979dd
Albert <Albertpm95@gmail.com>
2018-11-14 12:06:11 +0100

    Inicio practica 4

---
+++ Practica 4/Practica 4/src/practica/pkg4/ProductNotFoundException.java
@@ -0,0 +1,20 @@
+package practica.pkg4;/**
+ * Exception thrown when a product is not found in a shopping cart.
+ * @author Mike Clark
+ * @author Clarkware Consulting, Inc. 
+ */
+ 
+public class ProductNotFoundException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructs a ProductNotFoundException.
+	 */
+	public ProductNotFoundException() {
+		super();
+	}
+}
