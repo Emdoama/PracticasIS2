6e7e19ef938bbb4f50e53b9b1d58547cffade430
Albert <Albertpm95@gmail.com>
2018-11-26 17:15:01 +0100

    Emili garrulaire

--- Practica 4/Practica 4/test/practica/pkg4/ShoppingCartTest.java
+++ Practica 4/Practica 4/test/practica/pkg4/ShoppingCartTest.java
@@ -8,6 +8,7 @@ package practica.pkg4;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -20,6 +21,11 @@ import org.junit.Test;
 public class ShoppingCartTest {
     
     public ShoppingCartTest() {
+        ShoppingCart carrito = new ShoppingCart();
+        
+        assertTrue(carrito.isEmpty());
+   
+                
     }
     
     @BeforeClass
@@ -89,11 +95,18 @@ public class ShoppingCartTest {
      */
     @Test
     public void testRemoveItem() throws Exception {
-        System.out.println("removeItem");
-        Product p = null;
+        System.out.println("Test funcion borrar objeto. Removeitem()\n");
+        Product p = new Product("Producto", 1);
+        
         ShoppingCart instance = new ShoppingCart();
-        instance.removeItem(p);
-        // TODO review the generated test code and remove the default call to fail.
+        instance.addItem(p);
+        
+        try{
+            instance.removeItem(p); 
+            
+        }catch( Exception e){fail("No se ha podido remover el objeto p.");}
+
+        
         //fail("The test case is a prototype.");
     }
 
@@ -102,13 +115,22 @@ public class ShoppingCartTest {
      */
     @Test
     public void testGetItemCount() {
-        /*System.out.println("getItemCount");
+        System.out.println("Test obtener cuenta carrito | GetItemCount");
         ShoppingCart instance = new ShoppingCart();
         int expResult = 0;
         int result = instance.getItemCount();
-        assertEquals(expResult, result);*/
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        Product p, p1;
+
+        assertEquals(expResult, result);
+        
+        p = new Product("Producto", 0);
+        p1 = new Product("Producto 1", 1);
+        
+        instance.addItem(p);
+        instance.addItem(p1);
+        result = instance.getItemCount();
+
+        assertEquals(2, result);
     }
 
     /**
@@ -116,11 +138,20 @@ public class ShoppingCartTest {
      */
     @Test
     public void testEmpty() {
-        /*System.out.println("empty");
+        System.out.println("Test vaciar carrito. | testEmpty() | ");
         ShoppingCart instance = new ShoppingCart();
-        instance.empty();*/
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        int result = instance.getItemCount();
+        Product p, p1;
+        p = new Product("Producto", 0);
+        p1 = new Product("Producto 1", 1);
+        assertEquals(0, result);
+        instance.addItem(p);
+        instance.addItem(p1);
+        result = instance.getItemCount();
+        assertEquals(2, result);
+        instance.empty();
+        result = instance.getItemCount();
+        assertEquals(0, result);
     }
 
     /**
@@ -128,13 +159,15 @@ public class ShoppingCartTest {
      */
     @Test
     public void testIsEmpty() {
-       /* System.out.println("isEmpty");
+        System.out.println("Test comprovacion si Vacio | isEmpty()");
         ShoppingCart instance = new ShoppingCart();
-        boolean expResult = false;
+        Product p, p1;
+        p = new Product("Producto", 0);
+        p1 = new Product("Producto 1", 1);
+        instance.addItem(p);
+        instance.addItem(p1);
         boolean result = instance.isEmpty();
-        assertEquals(expResult, result);*/
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        assertEquals(false, result);
     }
 
     /**
@@ -155,14 +188,22 @@ public class ShoppingCartTest {
      */
     @Test
     public void testFindProduct() {
-        /*System.out.println("findProduct");
-        String titulo = "";
+        System.out.println("Test encontrar producto | FindProduct()");
+        String titulo = "Producto";
+        String titulo2 = "Producto 1";
+        String titulo3 = "Esteproductonoesta";
         ShoppingCart instance = new ShoppingCart();
-        boolean expResult = false;
+        Product p, p1;
+        p = new Product("Producto", 0);
+        p1 = new Product("Producto 1", 1);
+        instance.addItem(p);
+        instance.addItem(p1);
         boolean result = instance.findProduct(titulo);
-        assertEquals(expResult, result);*/
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        boolean result2 = instance.findProduct(titulo2);
+        boolean result3 = instance.findProduct(titulo3);
+        assertEquals(true, result);
+        assertEquals(true, result2);
+        assertEquals(false, result3);
     }
     
     
@@ -189,5 +230,31 @@ public class ShoppingCartTest {
             fail("Ha fallado la funcion FindProduct. | p2.getTitle() | testAddItemFindProduct()\n"); 
         if(!(instance.findProduct(p3.getTitle())))
             fail("Ha fallado la funcion FindProduct. | p3.getTitle() | testAddItemFindProduct()\n");
+    }
+    
+    @Test
+    public void testRemoveItem2() throws Exception {
+        System.out.println("Test funcion borrar objeto. Removeitem()\n");
+        Product p = new Product("Producto", 1);
+        
+        ShoppingCart instance = new ShoppingCart();
+        instance.addItem(p);
+        instance.removeItem(p);
+        
+        assertTrue(instance.isEmpty()); 
+    }
+    
+    @Test
+    public void testRemoveItemCarroVacio() throws Exception {
+        System.out.println("Test funcion borrar objeto. Removeitem()\n");
+        Product p = new Product("Producto", 1);
+        
+        ShoppingCart instance = new ShoppingCart();
+
+        try
+        {
+            instance.removeItem(p);
+            fail("No ha saltado la excepcion de removeItem().");
+        }catch( Exception e){}
     }
 }
